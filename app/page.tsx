d7395d6c7715c07269567b362ff472cff112b153
ROY RUBIN <royrubin@Mac.localdomain>
2025-11-26 19:03:45 -0800

    Fix DashboardView import in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { DashboardView } from '@/components/dashboard/DashboardView';
+import DashboardView from '@/components/dashboard/DashboardView';
 
 export default function Home() {
     return (
